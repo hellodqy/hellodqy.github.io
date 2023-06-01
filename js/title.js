c0cabfee6b9aee17773106d3ec33cb8fe8e9826c
dqy <1016751306@qq.com>
2023-06-01 11:23:48 +0800

    Site updated: 2023-06-01 11:23:47

--- js/title.js
+++ js/title.js
@@ -1,17 +1,1 @@
-//动态标题
-var OriginTitile = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-    if (document.hidden) {
-        //离开当前页面时标签显示内容
-        document.title = '404 Not Found';
-        clearTimeout(titleTime);
-    } else {
-        //返回当前页面时标签显示内容
-        document.title = '骗你哒~';
-        //两秒后变回正常标题
-        titleTime = setTimeout(function () {
-            document.title = OriginTitile;
-        }, 2000);
-    }
-});
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="404 Not Found",clearTimeout(titleTime)):(document.title="骗你哒~",titleTime=setTimeout((function(){document.title=OriginTitile}),2e3))}));
